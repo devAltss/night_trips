f12c55a27522c5f9d59a1e02aa9ac6f9e3758f1d
devAltss <altoss11@gmail.com>
2021-06-05 01:19:21 +0300

    added pick and about

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,41 @@
 $(function () {
+  $('.header-top__btn').on('click', function () {
+    $('.rightside-menu').removeClass('rightside-menu--open');
+  })
+  $('.rightside-menu__close').on('click', function () {
+    $('.rightside-menu').addClass('rightside-menu--open');
+  })
+
+
+  $('.pick__tabss .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.pick__tabss').find('.tab-item').removeClass('active-tab').hide();
+      $('.pick__tabss .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });
+
+
+    $('.tab-inner__slider').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      navText: ['<img src="images/arrow-prev.svg">', '<img src="images/arrow-next.svg">'],
+      //dots:true,
+      dotsEach:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+  })
+
 
 })
--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ function images() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/owl.carousel/dist/owl.carousel.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
